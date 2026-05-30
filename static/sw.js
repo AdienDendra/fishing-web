78ab9bd705e53ac3a916cc1829a5eb58056314a5
Adien Dendra <adien.dendra@gmail.com>
2026-05-30 21:11:09 +1000

    edit PWA

--- static/sw.js
+++ static/sw.js
@@ -1,3 +1,27 @@
-self.addEventListener('fetch', function(event) {
-  // Biarkan kosong untuk memenuhi syarat installability Chrome
+const CACHE_NAME = 'blowfish-pwa-cache-v1';
+const ASSETS = [
+  '/',
+  '/offline/',
+  '/manifest.webmanifest'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Strategies
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(e.request);
+    })
+  );
 });
